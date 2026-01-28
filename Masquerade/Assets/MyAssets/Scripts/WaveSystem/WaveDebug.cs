fe622655a864be97b4b0ba906ddae2268d3c6a1b
UnusualSheep <brooks.shane1991@gmail.com>
2026-01-28 23:47:04 +0000

    Spawning
    Spawning looking good so far, working on wave transition next

---
+++ Masquerade/Assets/MyAssets/Scripts/WaveSystem/WaveDebug.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class WaveDebug : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.K))
+        {
+            WaveManagement.Instance.EnemyDied();
+        }
+        if(Input.GetKeyDown(KeyCode.N))
+        {
+            WaveManagement.Instance.StartNextWave();
+        }
+    }
+}
---
+++ Masquerade/Assets/MyAssets/Scripts/WaveSystem/WaveManagement.cs
@@ -0,0 +1,142 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class WaveManagement : MonoBehaviour
+{
+
+    public static WaveManagement Instance { get; private set; }
+
+    //wave tracker
+    public int currentWave = 0;
+    public float waveModifier = 1.0f;
+    public float modifierIncreasePerWave = 0.2f;
+
+    public int baseEnemyCount = 5;
+    public int enemyIncreasePerWave = 3; //this is just a default value, will be math
+
+    //enemies
+    public List<EnemySpawnInfo> enemyTypes;
+    public Transform[] spawnPoints;
+
+    private int m_enemiesAlive = 0;
+    private bool m_waveActive = false;
+
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+    }
+
+    public void StartNextWave()
+    {
+        if (m_waveActive) return;
+
+        currentWave++;
+        waveModifier = 1.0f + (currentWave - 1) * modifierIncreasePerWave;
+
+        int enemiesToSpawn = baseEnemyCount + (currentWave -1) * enemyIncreasePerWave; // again this is temp!
+
+        StartCoroutine(SpawnWave(enemiesToSpawn));
+    }
+
+    public void EnemyDied()
+    {
+        m_enemiesAlive--;
+        if(m_enemiesAlive <= 0)
+        {
+            EndWave();
+        }
+    }
+
+    IEnumerator SpawnWave(int _count)
+    {
+        m_waveActive = true;
+
+        for(int i = 0; i < _count; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.5f); // just a small delay to try and make it so enemies don't all spawn on top of eachother
+        }
+    }
+
+    void SpawnEnemy()
+    {
+        EnemySpawnInfo spawnInfo = GetRandomEnemyForWave();
+        Transform spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)];
+
+        GameObject enemy = Instantiate(spawnInfo.enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+        enemy.GetComponent<EnemyTemp>().Initialize(waveModifier);
+
+        m_enemiesAlive++;
+    }
+
+
+    EnemySpawnInfo GetRandomEnemyForWave()
+    {
+        float totalweight = 0f;
+        List<(EnemySpawnInfo enemy, float weight)> weightedList = new List<(EnemySpawnInfo enemy, float weight)>();
+
+        foreach(var enemy in enemyTypes)
+        {
+            float weight = CalculateWeight(enemy);
+            if (weight <= 0f) continue;
+
+            weightedList.Add((enemy, weight));
+            totalweight += weight;
+        }
+
+        float roll = Random.Range(0, totalweight);
+        float cumulative = 0f;
+
+        foreach(var entry in weightedList)
+        {
+            cumulative += entry.weight;
+            if (roll <= cumulative)
+                return entry.enemy;
+        }
+
+        return weightedList[0].enemy;
+    }
+
+    float CalculateWeight(EnemySpawnInfo enemy)
+    {
+        int wavesSinceUnlock = currentWave - enemy.minWave;
+        if (wavesSinceUnlock < 0)
+            return 0f;
+
+        float weight = enemy.baseWeight;
+
+        // decay phase
+        if (wavesSinceUnlock >= enemy.growthDelayWaves)
+        {
+            int growthWaves = wavesSinceUnlock - enemy.growthDelayWaves + 1;
+            weight += enemy.bonusWeightPerWave * growthWaves;
+        }
+
+        //decay phase
+        if (wavesSinceUnlock >= enemy.decayStartWaves)
+        {
+            int decayWaves = wavesSinceUnlock - enemy.decayStartWaves + 1;
+            weight -= enemy.decayPerWave * decayWaves;
+        }
+
+        return Mathf.Clamp(weight, enemy.minWeight, enemy.maxWeight);
+    }
+
+    void EndWave()
+    {
+        m_waveActive = false;
+        Debug.Log($"Wave {currentWave} complete!"); 
+
+        //TODO
+        //enable end of round stuff here
+    }
+
+}
